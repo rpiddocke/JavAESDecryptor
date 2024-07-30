b32eb936cfd606971f967b20e8797f7d762766b0
rpiddocke <github@piddocke.com>
2024-07-30 16:25:29 -0700

    First commit

--- src/AESEncryptionApp.java
+++ src/AESEncryptionApp.java
@@ -58,28 +58,6 @@ public class AESEncryptionApp {
         }
     }
 
-    public static int AESEncrypt(String lpwszSource, StringBuilder lpwszDest, String lpwszKey) {
-        try {
-            // Hash the encryption key using SHA-256
-            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-            byte[] keyHash = sha256.digest(lpwszKey.getBytes("UTF-16LE"));
-
-            // Encrypt lpwszSource with AES in ECB mode
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            SecretKeySpec secretKeySpec = new SecretKeySpec(keyHash, "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            byte[] sourceBytes = lpwszSource.getBytes("UTF-16LE");
-            byte[] encrypted = cipher.doFinal(sourceBytes);
-
-            // Base64 encode the encrypted data
-            lpwszDest.append(Base64.getEncoder().encodeToString(encrypted));
-            return CRYPT_OK;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return CRYPT_ERROR;
-        }
-    }
-
     public static String AESDecrypt(String lpwszSource, String lpwszKey) {
         try {
             // Hash the encryption key using SHA-256
